480ae6d9b041a288af2564dc197b88c7d2ff2be6
DamSiedlecki <damsiedlecki.dev@gmail.com>
2024-01-03 13:25:31 +0100

    Revenue model data annotations

--- BarBalanceAPI/Models/Revenue.cs
+++ BarBalanceAPI/Models/Revenue.cs
@@ -1,4 +1,5 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BarBalanceAPI.Models
 {
@@ -6,20 +7,44 @@ namespace BarBalanceAPI.Models
     {
         public int ID { get; set; }
         public DateOnly ShiftDate { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal CashOpening { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal SafeOpening { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal CashReceived { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal CashWithdrawn { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal CashClosing { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal SafeClosing { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal CardPayments { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal DailyReport { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal InternalExpenditure { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal InvoicesWithoutFiscalization { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal CardTips { get; set; }
 
-        // Additional calculations
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal DailyRevenueTotal => InternalExpenditure + DailyReport;
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal DailyIncome => DailyRevenueTotal + InvoicesWithoutFiscalization;
     }
 }
